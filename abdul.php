3c003133ecec70cd6a4f8add006343a5a66a60f6
abdulhakims <160507011+abdulhakims@users.noreply.github.com>
2025-07-17 08:15:32 +0800

    Add files via upload

---
+++ abdul.php
@@ -0,0 +1,16 @@
+<?php
+echo "My name is: Abdul Hakim B. Palo <br>";
+echo "I am 18 years old <br>";
+echo "My favorite subject is: Computer Programming <br>";
+echo "My dream job is: Software Engineer <br>";
+echo "My motto in 'life It is what it is' <br>";
+echo "Here is my  ASCII art <br>";
+echo " <pre>";
+echo " ,-.___,-.    /\_/\  (     <br>";
+echo " \_/_ _\_/   ( ^.^ ) _)      <br>";
+echo "   )O_O(       \'/  (    <br>";
+echo "  { (_) }    ( | | )     <br>";
+echo "   `-^-'    (__d b__)         ";
+echo " </pre>";
+
+?>
